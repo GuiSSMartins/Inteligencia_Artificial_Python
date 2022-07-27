cd11eab206de747735141a233d150abbcd698447
GuiSSMartins <60627482+GuiSSMartins@users.noreply.github.com>
2022-07-27 22:53:01 +0100

    Create ArvoreGenealogica.pl

---
+++ Exemplos/ArvoreGenealogica.pl
@@ -0,0 +1,18 @@
+% factos
+progenitor(maria,joao).
+progenitor(jose,joao).
+progenitor(maria,ana).
+progenitor(jose,ana).
+progenitor(joao,mario).
+progenitor(ana,helena).
+progenitor(ana,joana).
+progenitor(helena,carlos).
+progenitor(mario,carlos).
+sexo(ana,feminino).
+sexo(maria,feminino).
+sexo(joana,feminino).
+sexo(helena,feminino).
+sexo(mario,masculino).
+sexo(joao,masculino).
+sexo(jose,masculino).
+sexo(carlos,masculino).
