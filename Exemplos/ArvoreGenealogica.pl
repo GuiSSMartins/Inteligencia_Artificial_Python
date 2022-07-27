bb9f2d303fcdf61f9af91962f4e6223953a65e49
GuiSSMartins <60627482+GuiSSMartins@users.noreply.github.com>
2022-07-27 23:15:44 +0100

    Update ArvoreGenealogica.pl

--- Exemplos/ArvoreGenealogica.pl
+++ Exemplos/ArvoreGenealogica.pl
@@ -8,6 +8,7 @@ progenitor(ana,helena).
 progenitor(ana,joana).
 progenitor(helena,carlos).
 progenitor(mario,carlos).
+
 sexo(ana,feminino).
 sexo(maria,feminino).
 sexo(joana,feminino).
@@ -16,3 +17,32 @@ sexo(mario,masculino).
 sexo(joao,masculino).
 sexo(jose,masculino).
 sexo(carlos,masculino).
+
+irma(X,Y):- progenitor(A,X),
+progenitor(A,Y),
+X\==Y,
+sexo(X,feminino).
+irmao(X,Y):- progenitor(A,X),
+progenitor(A,Y),
+X\==Y,
+sexo(X,masculino).
+descendente(X,Y):- progenitor(X,Y).
+descendente(X,Y):- progenitor(X,A),
+descendente(A,Y).
+avo(X,Y):- progenitor(X,A),
+progenitor(A,Y),
+sexo(X,masculino).
+mae(X,Y):- progenitor(X,Y),
+sexo(X,feminino).
+pai(X,Y):- progenitor(X,Y),
+sexo(X,masculino).
+tio(X,Y):- irmao(X,A),
+progenitor(A,Y).
+primo(X,Y):-irmao(A,B),
+progenitor(A,X),
+progenitor(B,Y),
+X\==Y.
+primo(X,Y):-irma(A,B),
+progenitor(A,X),
+progenitor(B,Y),
+X\==Y.
